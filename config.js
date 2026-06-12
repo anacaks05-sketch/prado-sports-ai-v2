b93ba9b6818a14f58d232099259e65bb01bacda1
anacaks05-sketch <ana.c.aks05@gmail.com>
2026-06-12 17:21:43 -0300

    Update API_KEY in config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const PRADO_CONFIG = {
   PROVIDER: 'api-football',
 
-  API_KEY: '508c2ae3d61ffd21296011f844cf9154',
+  API_KEY: 'COLE_SUA_CHAVE_AQUI',
 
   API_HOST: 'v3.football.api-sports.io',
   API_BASE_URL: 'https://v3.football.api-sports.io',
